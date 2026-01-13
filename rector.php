5ee2e5e2fc3dfee0ab5cc86269fc405579ea2caa
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2026-01-13 11:48:40 +0200

    % apply cs fixer

--- rector.php
+++ rector.php
@@ -1,14 +1,15 @@
 <?php
+
 declare(strict_types=1);
 
-use Rector\Config\RectorConfig;
 use IfCastle\CodeStyle\Rector\RectorConfigurator;
+use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-    
-    (new RectorConfigurator)->configureSets($rectorConfig);
+
+    new RectorConfigurator()->configureSets($rectorConfig);
 };
